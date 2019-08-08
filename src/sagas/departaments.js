05a31053de3b24e57a40a60b947bf859b974da1a
José Junior <junioradorart@gmail.com>
2019-08-08 16:36:16 -0300

    feat: fix invalid json errors on mock data

--- src/sagas/departaments.js
+++ src/sagas/departaments.js
@@ -1,8 +1,17 @@
-import {all, takeLatest} from 'redux-saga/effects';
-import {FETCH_DEPARTMENTS} from "../ducks/departments";
+import {all, takeLatest, put, call} from 'redux-saga/effects';
+import {FETCH_DEPARTMENTS, fetchDepartmentsFailed} from "../ducks/departments";
+import {DepartamentsAPI} from "../integrations/DepartamentsAPI";
+
+const departamentsApi = new DepartamentsAPI();
 
 function* fetchDepartamentsSaga() {
-  alert('entrou')
+  try {
+    const departaments = yield call(departamentsApi.getDepartaments);
+    console.log('departamentos', departaments)
+  } catch (e) {
+    console.log(e)
+    put(fetchDepartmentsFailed());
+  }
 }
 
 function* watchFetchDepartaments() {
