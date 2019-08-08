e4fe5bc01e44b07107b514ebb754b8e9f9569468
José Junior <junioradorart@gmail.com>
2019-08-07 23:05:57 -0300

    feat: fix Button style

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,12 +4,13 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import Button from "../components/button/Button";
 import CompareIcon from "../components/icons/CompareIcon";
+import "../main.scss";
 
 storiesOf('Button', module)
-  .addDecorator(storyFn => <div style={{ margin: '15px' }}>{storyFn()}</div>)
+  .addDecorator(storyFn => <div style={{ margin: '15px', fontSize: '16px' }}>{storyFn()}</div>)
   .add('without type or icon attributes', () => <Button label='Meu botão' onClick={action('click handler')} />)
   .add('with type "secondary"', () => <Button type='secondary' label='Meu botão' onClick={action('click handler')} />)
   .add('with type "primary"', () => <Button type='primary' label='Meu botão' onClick={action('click handler')} />)
   .add('with an icon', () => <Button type='primary' icon={<CompareIcon />} label='Meu botão' onClick={action('click handler')} />)
-  .add('with highlighted text', () => <Button type='primary' highlighted icon={<CompareIcon />} label='Meu botão' onClick={action('click handler')} />)
+  .add('with highlighted text', () => <Button type='primary' highlight icon={<CompareIcon />} label='Meu botão' onClick={action('click handler')} />)
   .add('with block style', () => <Button type='primary' highlight block icon={<CompareIcon />} label='Meu botão' onClick={action('click handler')} />)
