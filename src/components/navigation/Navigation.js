565c8c23aeabd971e8edd04b9f6c3686ed69c602
José Junior <junioradorart@gmail.com>
2019-08-09 00:25:24 -0300

    feat: add main-navigation to Navigation component

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,18 +1,22 @@
-import React, { memo, Fragment } from 'react';
+import React, { memo, Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Button from "../button/Button";
 import CompareIcon from "../icons/CompareIcon";
 import CloseIcon from "../icons/CloseIcon";
 import {appPrefix} from "../../constants/settings";
+import { connect } from 'react-redux';
 
 const menuClassName = `${appPrefix}menu`;
 function Navigation(
   {
     open = false,
-    onClose = () => {}
+    onClose = () => {},
+    departments,
   }
 ) {
+  const [showAllDepartments, setShowAllDepartments] = useState(false);
+
   const classNames = classnames({
     [`${menuClassName}`]: true,
     [`${menuClassName}--is-open`]: open !== false,
@@ -27,6 +31,35 @@ function Navigation(
   const onCloseHandler = (ev) => {
     ev.preventDefault();
     onClose();
+  };
+
+  const showAllDepartmentsHandler = () => {
+    setShowAllDepartments(true);
+  };
+
+  const generateDynamicLinks = () => {
+    let filteredDepartments = departments.data;
+
+    if (!showAllDepartments) {
+      filteredDepartments = departments.data.filter(department => {
+        return department.FlagMenu;
+      })
+
+    }
+
+    return filteredDepartments
+      .map(departament => {
+        return (
+          <li key={departament.IdDepartamento} className={`${menuClassName}__main-navigation-item`}>
+            <a href="#" className={`${menuClassName}__main-navigation-link`}>
+                <span className={`${menuClassName}__main-navigation-icon`}>
+                  <CompareIcon />
+                </span>
+              <span className={`${menuClassName}__main-navigation-label`}>{departament.Nome}</span>
+            </a>
+          </li>
+        );
+      });
   }
 
   return (
@@ -42,7 +75,27 @@ function Navigation(
           </nav>
           <a href="#" onClick={onCloseHandler} className={`${menuClassName}__close-link`}><CloseIcon /></a>
         </header>
+        <ul className={`${menuClassName}__main-navigation`}>
+          <li className={`${menuClassName}__main-navigation-item`}>
+            <a href="#" className={`${menuClassName}__main-navigation-link`}>
+              <span className={`${menuClassName}__main-navigation-label`}>Home</span>
+            </a>
+          </li>
+          <li className={`${menuClassName}__main-navigation-item`}>
+            <h3 className={`${menuClassName}__main-navigation-title`}>Compre por departamentos</h3>
+          </li>
 
+          {generateDynamicLinks()}
+
+          {
+            departments.data.length && !showAllDepartments
+              && (
+                <li className={`${menuClassName}__main-navigation-sticky-item`}>
+                  <Button onClick={showAllDepartmentsHandler} block type='secondary' label='Veja todos os departamentos' />
+                </li>
+              )
+          }
+        </ul>
       </section>
       <div className={backdropClassNames}></div>
     </Fragment>
@@ -54,4 +107,10 @@ Navigation.propTypes = {
   onClose: PropTypes.func,
 };
 
-export default memo(Navigation);
+export default connect(
+  ({departments}) => {
+    return {
+      departments,
+    }
+  }
+)(memo(Navigation));
