3a08ac0674b171f1c697724cd164454a3a1c1618
José Junior <junioradorart@gmail.com>
2019-08-19 05:20:33 -0300

    feat: create new actions on products ducks file

--- src/ducks/products.js
+++ src/ducks/products.js
@@ -3,9 +3,13 @@ export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
 export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
 export const FETCH_PRODUCTS_FAILED = 'FETCH_PRODUCTS_FAILED';
 
+export const FETCH_SINGLE_PRODUCT = 'FETCH_SINGLE_PRODUCT';
+export const FETCH_SINGLE_PRODUCT_SUCCESS = 'FETCH_SINGLE_PRODUCT_SUCCESS';
+export const FETCH_SINGLE_PRODUCT_FAILED = 'FETCH_SINGLE_PRODUCT_FAILED';
 // INITIAL STATE
 const initialState = {
   data: [],
+  currentInView: null,
   isLoading: false,
   productInDetailMode: null,
 };
@@ -30,6 +34,22 @@ function products(state = initialState, action) {
         ...state,
         isLoading: false,
       };
+    case FETCH_SINGLE_PRODUCT:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FETCH_SINGLE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        currentInView: product,
+        isLoading: false,
+      };
+    case FETCH_SINGLE_PRODUCT_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+      };
     default:
       return {
         ...state
@@ -41,5 +61,9 @@ function products(state = initialState, action) {
 export const fetchProducts = () => ({type: FETCH_PRODUCTS});
 export const fetchProductsSuccess = products => ({type: FETCH_PRODUCTS_SUCCESS, products});
 export const fetchProductsFailed = () => ({type: FETCH_PRODUCTS_FAILED});
+
+export const fetchSingleProduct = () => ({type: FETCH_SINGLE_PRODUCT});
+export const fetchSingleProductSuccess = products => ({type: FETCH_SINGLE_PRODUCT_SUCCESS, products});
+export const fetchSingleProductFailed = () => ({type: FETCH_SINGLE_PRODUCT_FAILED});
 
 export default products;
